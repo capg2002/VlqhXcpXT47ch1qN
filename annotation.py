481f4ab57add85ae30e3436be5c4fccb744246c1
capg2002 <carlosapaezg@hotmail.com>
2026-09-04 10:10:35 -0400

    editing major code and adding back data for global usage.

--- annotation.py
+++ annotation.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-folder = Path("images/testing2/IMG_4028_frames_pt2")
-prefix = "0002_"
+folder = Path("images/IMG_4267_frames")
+prefix = "0004_"
 
 for path in folder.iterdir():
 
@@ -13,8 +13,21 @@ for path in folder.iterdir():
             path.with_name(new_name)
         )
 
-folder = Path("images/testing2/IMG_4029_frames")
-prefix = "0003_"
+folder = Path("images/testing3/flip")
+prefix = "0005_"
+
+for path in folder.iterdir():
+
+    if path.is_file():
+
+        new_name = prefix + path.name
+
+        path.rename(
+            path.with_name(new_name)
+        )
+
+folder = Path("images/testing3/notflip")
+prefix = "0005_"
 
 for path in folder.iterdir():
 
